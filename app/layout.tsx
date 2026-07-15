1128dd08a7501e9f0c27cdbb96e6473d2079db0e
Luca Montini <lucamontini@MacBook-Pro-4.local>
2026-07-15 14:38:36 +0800

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -77,6 +78,7 @@ export default function RootLayout({
   return (
     <html lang="it">
       <body>{children}</body>
+      <GoogleAnalytics gaId="G-Y9XVME9PRS" />
     </html>
   );
 }
