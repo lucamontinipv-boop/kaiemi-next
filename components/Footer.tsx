5da1f999d5997a964fe8a9b833acc50640194aad
Luca Montini <LA_TUA_EMAIL_GITHUB>
2026-07-23 15:46:31 +0800

    Add VAT number to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
+const VAT_NUMBER = "85.59.99";
+
 export default function Footer() {
   return (
     <footer>
-      <div className="container">Kaiemi Coaching — © 2026 Noemi. Tutti i diritti riservati.</div>
+      <div className="container">
+        <div>Kaiemi Coaching</div>
+        <div>Noemi Magnani — P. IVA {VAT_NUMBER}</div>
+        <div>© 2026 Tutti i diritti riservati.</div>
+      </div>
     </footer>
   );
 }
